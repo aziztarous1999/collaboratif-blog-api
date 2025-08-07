c5b97e79387e9c56df7acfe68c0f6c045f4b617f
Aziz Tarous <56452240+aziztarous1999@users.noreply.github.com>
2025-08-07 19:25:40 +0100

    adding a Rate Limiting on login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-
+const apiLimiter = require('../middlewares/rateLimiter');
 router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/login',apiLimiter, authController.login);
 router.post('/refresh', authController.refresh);
 
 module.exports = router;
